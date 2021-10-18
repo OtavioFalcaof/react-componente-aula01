0bf388b2679ba781e1ff4cee3333614603e7f953
Otavio Augusto <otavio_falcao654@hotmail.com>
2021-10-17 21:01:32 -0300

    componente - aula01

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Message from './components/Message.js';
+import './index.css'
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Mensagem de esperança</h1>
+     <Message />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Message.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Message(){
+    return(
+        <div>
+            <pre>
+                Que nunca falte esperança a quem <br/>
+                tem sonhos maiores que a própria vida. 
+            </pre>
+        </div>
+    )
+}
